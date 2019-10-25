bf17b031bb511531d0570eab24c673599c8e75ea
Kelly DeMent <kdement@ufl.edu>
2019-10-25 18:35:27 -0400

    Immediately print temperatures to page

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 // It is always helpful to use comments in your code!
+
+const boilingTemp = 212;
+const freezingTemp = 32;
+let waterTemp = 85;
+
+//Immediately print boiling temp to boiling-temp span
+
+document.getElementById('boiling-temp').innerHTML = boilingTemp;
+
+//Immediately print freezing temp to freezing-temp span
+
+document.getElementById('freezing-temp').innerHTML = freezingTemp;
+
+//Immediately print water temp to water-temp span
+
+document.getElementById('water-temp').innerHTML = waterTemp;
+
+
+
+//Delay 3 seconds, remove hide class and add show class to correct Section
+
+//above 212, hide and show to boiling ID
+
+//below 32, hide and show to frozen ID
+
+//in between 32 and 212, hide and show to good-temp ID
